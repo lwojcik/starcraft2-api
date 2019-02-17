3d25b5d05d3d223ae0741cc37c4208b5317ed1cd
Łukasz Wójcik <lwojcik@lukem.net>
2019-02-17 14:37:47 +0100

    add test placeholder

---
+++ tests/index.test.ts
@@ -0,0 +1,9 @@
+import * as StarCraft2API from '../src/index';
+
+/* tslint:disable no-expression-statement */
+describe('Index', () => {
+  test('should export StarCraft2API class', () => {
+    expect(StarCraft2API).toBeDefined();
+  });
+});
+/* tslint:enable no-expression-statement */
