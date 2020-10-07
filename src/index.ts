629f9da61d5ea2780d0b4fa19567c912f9468f50
Łukasz Wójcik <lwojcik@lukem.net>
2020-10-07 21:18:58 +0200

    rewrite tests

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export { StarCraft2APIOptions, PlayerObject };
 export default StarCraft2API;
 
 /* istanbul ignore next */
-// tslint:disable no-object-mutation
 if (module) module.exports = StarCraft2API;
