ac7315f18a51467a98015dad21351993c553bb13
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2020-10-07 17:17:44 +0200

    add checkIfProfileUrlLooksValid + rewrite validateProfileUrl + modify profileUrlRegex (#181)
    * add checkIfProfileUrlLooksValid + rewrite validateProfileUrl
    
    * modify profileUrlRegex to accept only digits
    
    * fix tests for validateProfileUrl
    
    * minor refactoring of unpackProfileUrl

--- src/lib/helpers/profileUrls.ts
+++ src/lib/helpers/profileUrls.ts
@@ -13,34 +13,56 @@ export interface PlayerProfile {
 }
 
 export const profileUrlRegex =
-  /starcraft2\.com\/([A-z]{2}-[A-z]{2})\/profile\/(1|2|3|5{1})\/(1|2{1})\/([0-9]*)/i;
+  /starcraft2\.com\/([A-z]{2}-[A-z]{2})\/profile\/(1|2|3|5{1})\/(1|2{1})\/([0-9]\d*)/i;
 
-export const validateProfileUrl = (url: string) =>
+export const checkIfProfileUrlLooksValid = (url: string) =>
   profileUrlRegex.test(url);
 
 export const validateProfileId = (profileId: number | string) =>
   profileId.toString() === parseInt(profileId as string, 10).toString();
 
 export const unpackProfileUrl = (url: string, includeLocale?: boolean): PlayerProfile | {} => {
-  const urlIsValid = validateProfileUrl(url);
+  const urlIsValid = checkIfProfileUrlLooksValid(url);
 
   if (!urlIsValid) return {};
 
   const profileUrl = url.match(profileUrlRegex)![0];
   const profileDataArray = profileUrlRegex.exec(profileUrl)!;
 
-  const playerObject = {
+  return {
+    ...includeLocale && { locale: profileDataArray[1] },
     regionId: profileDataArray[2],
     realmId: profileDataArray[3],
     profileId: profileDataArray[4],
   };
+};
 
-  return includeLocale
-    ? {
-      locale: profileDataArray[1],
-      ...playerObject,
-    }
-    : playerObject;
+export const validateProfileUrl = (url: string, includeLocale?: boolean) => {
+  const profileObject = unpackProfileUrl(url, includeLocale) as PlayerProfile;
+
+  if (Object.keys(profileObject).length === 0) {
+    throw new RangeError(`${url} is not a valid argument for validateProfileUrl()`);
+  }
+
+  const {
+    regionId,
+    realmId,
+    profileId,
+  } = profileObject;
+
+  const localeName = includeLocale
+    ? (profileObject as PlayerProfile & { locale: string }).locale
+      : undefined;
+
+  const validRegionId = BlizzAPI.validateRegionId(regionId);
+  const validProfileId = validateProfileId(profileId);
+  const validRealmId = BlizzAPI.checkIfSc2RealmLooksValid(realmId);
+  const validLocale = localeName && validateProfileUrlLocale(localeName);
+
+  return validRegionId
+    && validRealmId
+    && validProfileId
+    && includeLocale ? validLocale : true;
 };
 
 export const constructProfileUrl = (
